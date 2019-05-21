12bfb41786544a78b28bf365ea39cbcf1b6759da
Jakob Uttenthal Israelsen <jais@itu.dk>
2019-05-21 14:50:30 +0200

    Added a template

---
+++ components/template.js
@@ -0,0 +1,18 @@
+'use strict';
+import BaseComponent from "../../lib/BaseComponent.js";
+
+export default class AddButton extends BaseComponent {
+    html = `
+        <div id="root"></div>
+    `;
+
+    script = () => {
+
+    };
+
+    style = `
+        #root {
+        
+        }
+    `;
+}
