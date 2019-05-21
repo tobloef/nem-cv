bd3ad0b06eed9bd3a5cb94dc1e1a627f920635e8
Jakob Uttenthal Israelsen <jais@itu.dk>
2019-05-21 12:33:42 +0200

    Can toggle editing on page

---
+++ components/temporary/edit-toggle.js
@@ -0,0 +1,12 @@
+function toggleAllEditors() {
+    const editors = document.querySelectorAll(".editable");
+    const addButtons = document.querySelectorAll(".add-button");
+
+    function toggleEdit(item) {
+        const edit = "contenteditable";
+        item.setAttribute(edit, "" + !(item.getAttribute(edit) === "true"));
+    }
+    editors.forEach(toggleEdit);
+    addButtons.forEach(item => item.style.visibility === "hidden" ? item.style.display = "block" : item.style.display = "none");
+
+}
