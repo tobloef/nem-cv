b9d65c9c3921bd9f1e3cfe4b1e906e356787efd9
Jonas Lindenskov Nielsen <jonas@zafepay.com>
2019-05-21 11:36:06 +0200

    WIP

--- components/home/PageHome.js
+++ components/home/PageHome.js
@@ -1,10 +1,13 @@
-export default class PageHome extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML = `
-            <h1>NemCV</h1>
-            <a href="/theme">Get Started</a>
-        `;
-    }
+import AbstractComponent from '../../lib/AbstractComponent.js';
+
+export default class PageHome extends AbstractComponent {
+    html = `
+        <h1>NemCV</h1>
+        <a href="/theme">Get Started</a>
+        <a href="/about">About</a>
+    `;
+
+    script() {}
 }
 
 customElements.define('page-home', PageHome);
