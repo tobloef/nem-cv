6fa30a8ae088a7ccaf324c893dd7e7a2e044813c
Jonas Lindenskov Nielsen <jonas@zafepay.com>
2019-05-21 10:57:21 +0200

    Split theme selection and home

--- components/home/PageHome.js
+++ components/home/PageHome.js
@@ -2,21 +2,7 @@ export default class PageHome extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <h1>NemCV</h1>
-            <b>Choose a theme</b>
-            <a href="/editor/red">Red theme</a>
-            <a href="/editor/blue">Blue theme</a>
-            <a href="/editor/green">Green theme</a>
-            <a href="/editor/purple">Purple theme</a>
-            <a href="/editor/special">Special theme</a>
-            <a href="/editor/invalid">Invalid theme</a>
-            <br>
-            <b>Other pages</b>
-            <a href="/about">About</a>
-            <a href="/invalidlink">Broken link</a>
-
-            <b>External pages</b>
-            <a href="https://itu.dk">ITU</a>
-            <a href="https://google.com">Google</a>
+            <a href="/theme">Get Started</a>
         `;
     }
 }
