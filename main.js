92d751b674fec3ff4e36244b82804b770b911b77
Tobias Løfgren <tobloef@gmail.com>
2019-05-21 17:53:25 +0200

    Refactor routing

--- main.js
+++ main.js
@@ -1,34 +1,3 @@
-import Router from "./lib/Router.js";
-import Pages from "./lib/Pages.js";
-import RootPages from "./components/RootPages.js"
+import RootRoutes from "./components/RootRoutes.js"
 
-RootPages.define();
-
-const root = document.querySelector('root-pages');
-
-const pages = new Pages();
-
-//Set router mode. Can either be history (use pure urls) or hash (uses a # before the url).
-
-//Register pages
-pages.add("home", root.shadowRoot.querySelector("page-home"))
-    .add("templates", root.shadowRoot.querySelector("page-templates"))
-    .add("editor", root.shadowRoot.querySelector("page-editor"))
-    .add("not-found", root.shadowRoot.querySelector("page-not-found"));
-
-//Register routes
-Router.add(/editor/, args => {
-    console.log("Router hit \"editor\"");
-    pages.show("editor");
-});
-Router.add(/templates/, () => {
-    console.log("Router hit \"templates\"");
-    pages.show("templates");
-});
-Router.add("", () => {
-    if (Router.currentUrl === "") {
-        return pages.show("home");
-    }
-    pages.show("not-found");
-});
-Router.listen();
+RootRoutes.define();
