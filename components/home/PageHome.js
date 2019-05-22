11e2decd3e3b93923a94482e122bacbcc229e54c
matildehorlyk <mhsv@itu.dk>
2019-05-22 18:38:36 +0200

    Tilføjelse af Buttons

--- components/home/PageHome.js
+++ components/home/PageHome.js
@@ -1,10 +1,19 @@
 import BaseComponent from "../BaseComponent.js";
 import RouterLink from "../shared/RouterLink.js";
+import CustomButton from "../shared/CustomButton.js";
 
 export default class PageHome extends BaseComponent {
     usedComponents = [
-        RouterLink
+        RouterLink,
+        CustomButton
     ];
+
+    // language=CSS
+    style = `
+        custom-button {
+            width: 200px;
+        }
+    `;
 
     get html() {
         return `
@@ -15,6 +24,19 @@ export default class PageHome extends BaseComponent {
               <router-link href="/editor">Editor</router-link>
               <router-link href="/blabla">Blabla (Not found)</router-link>
             </div>
+            <custom-button id="button1">Primary</custom-button>
+            <custom-button id="button2" secondary>Secondary</custom-button>
         `;
     };
+
+    script = () => {
+        const button1 = this.shadowRoot.getElementById("button1");
+        const button2 = this.shadowRoot.getElementById("button2");
+        button1.addEventListener("click", () => {
+            console.log("Primary");
+        });
+        button2.addEventListener("click", () => {
+            console.log("Secondary");
+        })
+    }
 }
