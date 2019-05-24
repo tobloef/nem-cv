a96a06c14fe9406e734c8a5e3f8db5103d970178
matildehorlyk <mhsv@itu.dk>
2019-05-24 13:32:13 +0200

    CV Modern færdig desktop

--- components/cvs/cv-modern/CVModern.js
+++ components/cvs/cv-modern/CVModern.js
@@ -8,6 +8,8 @@ export default class CVModern extends BaseComponent {
         ProfileImage
     ];
 
+
+
     // language=HTML
     get html() {
         return `
@@ -16,36 +18,41 @@ export default class CVModern extends BaseComponent {
                     <profile-image class="image" aspect_ratio="1.5"></profile-image>
                 </div>
                 <div class = "right">
-                    <h1>JENS HANSEN</h1>
+                    <h1>JENS</h1>
+                    <h1>HANSEN</h1>
                     <div class="color1"></div>
-                    <h2>BRANCHER</h2>
                     <ul class="listinfo">
-                        <li>1</li>
-                        <li>2</li>
-                        <li>3</li>
+                        <li>23 år</li>
+                        <li>København Ø</li>
+                        <li>mail@nemcv.me</li>
                     </ul>
                 </div>
             </header>
             
             <section class="about">
                 <div class="content">
-                    <div class="aboutfront">
-                        <h2>OM MIG</h2>
-                        <p class="text">Tekst om mig</p>
-
-                        <h2>BRANCHER</h2>
+                        <h2 class="aboutme">OM MIG</h2>
+                        <p class="text">Lorem ipsum dolor sit amet, 
+                            consectetur adipiscing elit, sed do eiusmod 
+                            tempor incididunt ut labore et dolore magna 
+                            aliqua. Ut enim ad minim veniam, quis nostrud 
+                            exercitation ullamco laboris nisi ut aliquip 
+                            ex ea commodo consequat. Duis aute irure dolor 
+                            in reprehenderit in voluptate velit esse cillum 
+                            dolore eu fugiat nulla pariatur. 
+                        </p>
+                        <h2 class="workareas">BRANCHER</h2>
                         <ul class = "listindustry">
                             <li>1</li>
                             <li>2</li>
                             <li>3</li>
                         </ul>
-                    </div>    
                 </div>
-                
             </section>
             
             <section class="education">
-                    <h2>UDDANNELSE</h2>
+                <div class="content">
+                    <h2 class="educationtitle">UDDANNELSE</h2>
                     <div class = "educations">
                         <div class = "oneeducation">
                             <div class="left1">
@@ -57,12 +64,12 @@ export default class CVModern extends BaseComponent {
                             </div>
                         </div>
                     </div>
+                </div>
             </section>
             
             <section class="experience">
-                <div class="color3"></div>
-                <div class="experiencefront">
-                    <h2>ERFARING</h2>
+                <div class="content">
+                    <h2 class="experiencetitle">ERFARING</h2>
                     <div class="experiences">
                         <div class="oneexperience">
                             <div class="left2">
@@ -87,18 +94,40 @@ export default class CVModern extends BaseComponent {
 
     // language=CSS
     get css() {
-        return `
+        return `            
+            :host{
+                font-family: 'Open Sans', sans-serif;
+                background-color: #F6F5EE;
+                display: block;
+            }
+            
+            ul{
+                list-style-type: disc;
+                padding-left: 2em;
+            }
+            
             header {
                 display: flex;
-                margin-bottom: 50px;
+                justify-content: space-between;
+                margin-bottom: 40px;
             }
             
             h1 {
+                font-size: 2em;
             }
 
             h2 {
+                font-size: 1.5em;
             }
             
+            li{
+                margin-bottom: 5px;
+            }
+
+            .image {
+                width: 400px;
+            }
+
             .about {
                 display: flex;
                 justify-content: flex-end;
@@ -114,49 +143,82 @@ export default class CVModern extends BaseComponent {
             .experience {
                 display: flex;
                 flex-direction: column;
-                margin-bottom: 50px;
+                justify-content: center;
+            }
+            
+            .aboutme {
+                margin-bottom: 30px;
             }
 
-            .text {
+            .workareas {
+                margin-bottom: 30px;
             }
 
             .left {
-                margin: 100px;
                 justify-content: flex-start;
-                max-width: 400px;
-                flex: 1 1 0px;
+                width: 100%;
+                padding: 110px;
+                height: 800px;
             }
 
             .right {
-                justify-content: flex-end;
+                display: flex;
+                width: 100%;
+                max-width: 400px;
+                flex-direction: column;
+                justify-content: center;
+            }
+            
+            .text{
+                /*max-width: 500px;*/
+                transform: translateX(-50%);
+                font-size: 0.9em;
+                margin-bottom: 30px;
             }
 
-            .listinfo {
+            .experiencetitle {
+                padding-left: -15em;
+                margin-bottom: 10px;
+                transform: translateX(20%);
+                text-align: right;
             }
-
-            .listindustry {
+ 
+            .educationtitle {
+                text-align: center;
+                margin-left: -15em;
+                margin-bottom: 10px;
             }
 
             .color1 {
                 height: 80px;
-                width: 200px;
-                background-color: #1d1c1a;
+                background-color: #302f2d;
                 position: relative;
+                margin-bottom: 30px;
             }
 
             .about .content {
+                padding-top: 50px;
+                padding-left: 50px;
+                padding-bottom: 50px;
+                padding-right: 50px;
+                max-width: 40%;
                 background-color: #E4E3D9;
             }
             
-            .color3{
+            .experience .content{
+                padding-top: 50px;
+                padding-left: 50px;
+                padding-bottom: 50px;
+                padding-right: 50px;
+                max-width: 40%;
+                background-color: #E4E3D9;
+            }
+
+            .color3 {
                 height: 200px;
                 width: 300px;
                 background-color: #E4E3D9;
                 position: relative;
-            }
-
-            .aboutfront {
-                /*position: absolute;*/
             }
 
             .experiencefront {
@@ -164,28 +226,61 @@ export default class CVModern extends BaseComponent {
             }
 
             .title {
+                font-size: 0.8em;
+                margin-bottom: 15px;
             }
 
             .place {
+                font-size: 1em;
+                margin-bottom: 5px;
             }
 
             .year {
+                font-size: 0.7em;
             }
 
             .educations {
+                margin-left: auto;
+                margin-right: auto;
+                width: 100%;
+                max-width: 30%;
             }
 
             .oneeducation {
                 display: flex;
-                justify-content: center;
+                justify-content: space-between;
+                border-top: 1px solid black;
+                padding: 5px;
             }
 
             .experiences {
+                transform: translateX(15%);
+                
             }
 
             .oneexperience {
                 display: flex;
-                justify-content: center;
+                justify-content: space-between;
+            }
+            
+            @media(max-width: 800px) {
+                :host{
+                    background-color: indianred;
+                }
+                
+                header{
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                }
+                
+                .right{
+                    justify-content: center;
+                }
+                
+                .left{
+                    justify-content: center;
+                }
             }
         `
     };
