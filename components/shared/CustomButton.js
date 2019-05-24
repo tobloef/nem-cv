cdd6c713da28f71a49a9d6d53ffcb14d088b49a3
Jonas Lindenskov Nielsen <jonas@zafepay.com>
2019-05-24 19:33:54 +0200

    Implemented alternative CTA on landing

--- components/shared/CustomButton.js
+++ components/shared/CustomButton.js
@@ -1,7 +1,7 @@
 import BaseComponent from "../BaseComponent.js";
 
 export default class CustomButton extends BaseComponent {
-    static observedAttributes = ["secondary", "inverted"];
+    static observedAttributes = ["secondary", "inverted", "solid"];
 
     // language=HTML
     get html() {
@@ -13,16 +13,18 @@ export default class CustomButton extends BaseComponent {
         return `
             :host {
                 display: flex;
-                
+
+                --background-color: transparent;
                 --border-color: black;
                 --text-color: black;
                 --hover-background-color: var(--border-color);
                 --hover-text-color: white;
+                --hover-border-color: transparent;
                 --border-thickness: 2px;
                 --padding-y: 14px;
                 --padding-x: 28px;
             }
-            
+
             :host([inverted]) {
                 --border-color: white;
                 --text-color: white;
@@ -30,10 +32,19 @@ export default class CustomButton extends BaseComponent {
                 --hover-text-color: black;
             }
 
+            :host([solid]) {
+                --background-color: black;
+                --text-color: white;
+
+                --hover-background-color: white;
+                --hover-text-color: black;
+                --hover-border-color: black;
+            }
+
             button {
                 width: 100%;
                 border: var(--border-thickness) solid var(--border-color);
-                background-color: transparent;
+                background-color: var(--background-color);
                 color: var(--text-color);
                 padding: var(--padding-y) var(--padding-x);
                 cursor: pointer;
@@ -48,6 +59,7 @@ export default class CustomButton extends BaseComponent {
             button:hover{
                 background-color: var(--hover-background-color);
                 color: var(--hover-text-color);
+                border-color: var(--hover-border-color);
             }
         `;
     };
