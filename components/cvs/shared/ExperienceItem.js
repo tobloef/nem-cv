f86624d2d5dd65dec05c0297e4f29e043a5fb6bf
Jakob Uttenthal Israelsen <jais@itu.dk>
2019-05-25 15:10:33 +0200

    Not sure what I did

--- components/cvs/shared/ExperienceItem.js
+++ components/cvs/shared/ExperienceItem.js
@@ -15,7 +15,7 @@ export default class ExperienceItem extends BaseComponent {
     get html() {
         return `
             <div content-type="object" class="container">
-                <span class="divider where" part="left">
+                <span class="divider where">
                     <${EditableText.elementName}
                             placeholder="${this.experienceType}"
                             element="div"
@@ -66,6 +66,9 @@ export default class ExperienceItem extends BaseComponent {
             }
             button {
                 display: inline;
+            }
+            .where {
+                padding-right: 3em;
             }
         `
     };
